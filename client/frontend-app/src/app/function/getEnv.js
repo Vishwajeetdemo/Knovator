65c302a993395ee2f20a299dd70ddb84d685f218
Vishwajeet Kumar <47150216+Vishwajeetdemo@users.noreply.github.com>
2025-11-09 16:25:28 +0530

    Update getEnv.js

--- client/frontend-app/src/app/function/getEnv.js
+++ client/frontend-app/src/app/function/getEnv.js
@@ -1,6 +1,6 @@
-function getAppUrl(appNodeEnv) {
-    const isProdOrStaging = appNodeEnv === 'production' || appNodeEnv === 'staging';
-    return isProdOrStaging ? process.env.REACT_APP_SITE_URL || '' : 'http://localhost:7000';
+function getAppUrl() {
+    const isProdOrStaging = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging';
+    return isProdOrStaging ? process.env.NEXT_PUBLIC_SITE_URL : 'http://localhost:7000';
 }
 
 function getReactAppUrl(appNodeEnv) {
